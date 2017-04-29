86528e24111c8485a474f7a47b930b9318b991ef
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2017-04-29 19:29:27 -0300

    Update prog1.lua

--- Lua/prog1.lua
+++ Lua/prog1.lua
@@ -5,9 +5,10 @@
 -- Abrimos o arquivo stop_words.txt para leitura
 local stop_words_file = io.open("../Documentos/stop_words.txt", "r")
 
--- Criamos uma lista vazia 
+-- Criamos uma lista vazia chamada stop_words
 local stop_words = {}
 
+-- Separamos as palavras de parada do arquivo stop_words.txt e as salvamos na lista stop_words
 while true do
         line = stop_words_file.read(stop_words_file)
     
@@ -20,7 +21,7 @@ while true do
         end
 end
 
--- Imprimimos as palavras de parada na tela
+-- Imprimimos as palavras de parada da lista stop_words na tela
 for i = 1, #stop_words do
       print (i, stop_words[i])
 end
