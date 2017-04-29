0dfb2985a15b2c3c0e76c9678bc387d77747215a
Daniela <daniela.brazao.maksoud@gmail.com>
2017-04-29 16:52:41 -0300

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import re, sys, collections
+
+stops = open('stop_words.txt').read().split(',')
+
+words = re.findall('[a-z]{2,}', open(sys.argv[1]).read().lower())
+
+counts = collections.Counter(w for w in words if w not in stops)
+
+for (w, c) in counts.most_common(25):
+ print (w, '-', c)
