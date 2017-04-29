6cec8de13ed7f3d290b5d329c7dc4543fcd40239
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2017-04-29 17:46:39 -0300

    Update prog1.lua

--- Lua/prog1.lua
+++ Lua/prog1.lua
@@ -3,7 +3,14 @@
 -- =/=/=/=/=/=/=/=/=/=/
 
 -- Abrimos o arquivo stop_words.txt para leitura
-io.open("Documentos/stop_words.txt", "r")
+local stop_words_file = io.open("Documentos/stop_words.txt", "r")
+
+-- Salvamos as palavras de parada em uma lista 
+if stop_words_file then
+    for line in stop_words_file:lines() do
+        local stop_words = unpack(line:split(","))
+    end
+end
 
 -- Fechamos o arquivo stop_words.txt
 io.input():close()
