ffc575b4e2484b776be496931a234c53afb27685
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2017-04-29 17:39:39 -0300

    Update prog1.lua

---
+++ Lua/prog1.lua
@@ -0,0 +1,10 @@
+-- =/=/=/=/=/=/=/=/=/=/
+-- C O D E  G O L F
+-- =/=/=/=/=/=/=/=/=/=/
+
+-- Abrimos o arquivo stop_words.txt para leitura
+io.open("Documentos/stop_words.txt", "r")
+
+-- Fechamos o arquivo stop_words.txt
+io.input():close()
+
