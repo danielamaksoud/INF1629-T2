a8f62b1c9459d16955ee3e57638e11e0693d67c5
Thaissa Falbo <thaissa.falbo@gmail.com>
2017-04-29 20:49:26 -0300

    prog2.lua stopwords mais facil

--- Lua/prog2.lua
+++ Lua/prog2.lua
@@ -13,25 +13,25 @@ local word_freqs = {}
 
 -- Abrimos o arquivo stop_words.txt para leitura
 local stop_words_file = io.open("../stop_words.txt", "r")
+local stop_words_text = stop_words_file:read"*all" -- Variavel para armazenar o texto
+
 
 -- Criamos uma tabela vazia chamada stop_words
 local stop_words = {}
 
 -- Separamos as palavras de parada do arquivo stop_words.txt e as salvamos na tabela stop_words
-while true do
-        line = stop_words_file.read(stop_words_file)
-    
-        if not line then 
-            break 
-        end
-    
-        for word in string.gmatch(line, "%a+") do
-            stop_words[#stop_words+1] = word
-        end
+stop_words = stop_words:gsub(",", " ")
+
+for word in stop_words:gmatch"%w+" do
+  table.insert(stop_words, word)
 end
 
 -- Fechamos o arquivo stop_words.txt
 stop_words_file:close()
+
+
+print(table.unpack())
+
 
 -- Abrimos o arquivo test.txt para leitura
 local test_file = io.open(arg[1], "r")
