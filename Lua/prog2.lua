52fc32bac48f72437eb7038d30f9503a88857761
Thaissa Falbo <thaissa.falbo@gmail.com>
2017-04-29 22:19:56 -0300

    prog2: inicia contador

--- Lua/prog2.lua
+++ Lua/prog2.lua
@@ -8,13 +8,16 @@ if #arg < 1 then
     return
 end
 
--- Criamos uma tabela vazia chamada word_freqs
-local word_freqs = {}
+-----
+-- TRATA ARQUIVO DE STOPWORDS
+------
 
 -- Abrimos o arquivo stop_words.txt para leitura
 local stop_words_file = io.open("../stop_words.txt", "r")
 local stop_words_text = stop_words_file:read"*all" -- Variavel para armazenar o texto
 
+-- Fechamos o arquivo stop_words.txt
+stop_words_file:close()
 
 -- Criamos uma tabela vazia chamada stop_words
 local stop_words = {}
@@ -26,12 +29,43 @@ for word in stop_words_text:gmatch"%w+" do
   table.insert(stop_words, word)
 end
 
--- Fechamos o arquivo stop_words.txt
-stop_words_file:close()
 
-
-print(table.unpack(stop_words))
-
+-----
+-- TRATA ARQUIVO DE TESTE E FREQUENCIA
+------
 
 -- Abrimos o arquivo test.txt para leitura
 local test_file = io.open(arg[1], "r")
+local test_text = test_file:read"*all" -- Variavel para armazenar o texto
+
+-- Fechamos o arquivo stop_words.txt
+test_file:close()
+
+-- Criamos uma tabela vazia chamada text_words
+local word_freqs = {}  
+
+test_text = test_text:gsub(","," ")
+
+for word in test_text:gmatch"%w+" do
+	if stop_words[word] then
+		print(word .. " is Stop Word")
+	else
+		print(word .. " is not Stop Word")
+	end
+end
+
+for word in test_text:gmatch"%w+" do
+    if not stop_words[word] then	
+  	    if not word_freqs[word] then	
+  	        word_freqs[word] = 0
+        end 
+    end 
+end 
+
+
+for c, v in pairs(word_freqs) do
+  print(c, v)
+end
+
+
+
