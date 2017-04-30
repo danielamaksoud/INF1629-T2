8f41438c5f05de030b1781f04b6402282c1da65a
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2017-04-29 23:03:43 -0300

    Update prog1.lua

--- Lua/prog1.lua
+++ Lua/prog1.lua
@@ -28,9 +28,9 @@ while true do
 end
 
 -- Imprimimos as palavras de parada da lista stop_words na tela
-print ("-----------------------------\nPalavras de Parada\n-----------------------------")
+print ("--------------------------------------------\nPalavras de Parada\n--------------------------------------------")
 for i = 1, #stop_words do
-      print (i, stop_words[i])
+      print (i," - ", stop_words[i])
 end
 
 -- Fechamos o arquivo stop_words.txt
@@ -55,10 +55,10 @@ while true do
         end
 end
 
--- Imprimimos as palavras de parada da lista stop_words na tela
-print ("-----------------------------\nPalavras de Teste\n-----------------------------")
+-- Imprimimos as palavras da lista words na tela
+print ("--------------------------------------------\nPalavras de Teste\n--------------------------------------------")
 for i = 1, #words, 1 do
-      print (i, words[i])
+      print (i," - ",words[i])
 end
 
 -- Fechamos o arquivo test.txt
@@ -67,9 +67,10 @@ test_file:close()
 -- Criamos uma lista vazia chamada counts
 counts = {}
 
+-- Inicializamos um contador de palavras de teste diferentes de palavras de parada
 j = 1
 
--- Comparamos as palavras de teste da lista words com as palavras de parada da lista stop_words e incluímos na lista counts aquelas que fazem parte das palavras de teste, mas não fazem parte das palavra de parada
+-- Comparamos as palavras de teste da lista words com as palavras de parada da lista stop_words e incluímos na lista counts aquelas que fazem parte das palavras de teste, mas não fazem parte das palavras de parada
 for i = 1, #words, 1 do
     for k = 1, #stop_words do
         if words[i] ~= stop_words[k] then
@@ -83,18 +84,21 @@ for i = 1, #words, 1 do
 end
 
 -- Imprimimos as palavras da lista counts na tela
-print ("-----------------------------\nPalavras\n-----------------------------")
+print ("--------------------------------------------\nPalavras de Teste sem Palavras de Parada\n--------------------------------------------")
 for i = 1, #counts, 1 do
-      print (i, counts[i])
+      print (i," - ",counts[i])
 end
 
 -- Criamos uma matriz
 mt = {}
 
+-- Inicializamos um contador do número de ocorrência de palavras repetidas na lista counts
 j = 1
 
+-- Inicializamos um contador de palavras removidas da lista counts
 c = 0
 
+-- Preenchemos a matriz mt com as palavras da lista counts e seus respectivos números de ocorrência
 for i = 1, #counts, 1 do
     for k = i+1, #counts, 1 do
         if counts[i] == counts[k] then
@@ -104,21 +108,55 @@ for i = 1, #counts, 1 do
         end
     end
     mt[#mt+1] = {counts[i], j}
+    -- mt[#mt+1] = tostring(j) .. counts[i]
     j = 1
 end
 
--- Imprimimos as palavras da lista counts na tela
-print ("-----------------------------\nPalavras\n-----------------------------")
+-- Imprimimos todas as palavras da lista counts na tela
+print ("--------------------------------------------\nPalavras Não Repetidas\n--------------------------------------------")
 for i = 1, #counts, 1 do
-      print (i, counts[i])
+      print (i, " - ", counts[i])
 end
 
--- Imprimimos as palavras da lista counts na tela
-print ("-----------------------------\nPalavras\n-----------------------------")
-for y = 1, #mt-c do
+-- Imprimimos as palavras da lista counts e seus respectivos números de ocorrência na tela
+print ("--------------------------------------------\nPalavras + Ocorrências\n--------------------------------------------\n")
+for y = 1, #mt-c, 1 do
     for x = 1, 2 do
-         print(mt[y][x])
+        if x == 1 then
+            print("Palavra:", mt[y][x])
+        else
+            print("Ocorrência:", mt[y][x])
+        end
     end
+    -- print(i, mt[i])
+    print ("\n-----------------------------\n")
+end
+
+for y = 1, #mt-c, 1 do
+    for k = y+1, #mt-c, 1 do
+        if (mt[y][2] < mt[k][2]) then
+            aux1 = mt[y][1]
+            aux2 = mt[y][2]
+            mt[y][1] = mt[k][1]
+            mt[y][2] = mt[k][2]
+            mt[k][1] = aux1
+            mt[k][2] = aux2
+        end
+    end
+end
+
+-- Imprimimos as palavras da lista counts e seus respectivos números de ocorrência na tela
+print ("--------------------------------------------\nPalavras + Ocorrências\n--------------------------------------------\n")
+for y = 1, #mt-c, 1 do
+    for x = 1, 2 do
+        if x == 1 then
+            print("Palavra:", mt[y][x])
+        else
+            print("Ocorrência:", mt[y][x])
+        end
+    end
+    -- print(i, mt[i])
+    print ("\n-----------------------------\n")
 end
 
 
