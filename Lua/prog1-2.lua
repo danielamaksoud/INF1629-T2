46327edfc0cc5a6048a95656dda9bf7e86c2c328
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2017-05-01 14:15:01 -0300

    Submit prog1-2.lua

---
+++ Lua/prog1-2.lua
@@ -0,0 +1,70 @@
+-- =/=/=/=/=/=/=/=/=/=/
+-- C O D E  G O L F
+-- =/=/=/=/=/=/=/=/=/=/
+
+local stop_words_file = io.open("../stop_words.txt", "r")
+local stop_words_text = stop_words_file:read("*all")
+stop_words_file:close()
+
+local stop_words = {}
+
+for word in stop_words_text:gmatch"%w+" do
+  stop_words[word] = true
+end
+
+local test_file = io.open(arg[1], "r")
+local test_text = test_file:read("*all"):lower()
+test_file:close()
+
+local word_freqs = {}  
+
+for word in test_text:gmatch"%w+" do
+    if not stop_words[word] then	
+  	    if not word_freqs[word] then	
+  	        word_freqs[word] = 1
+        else
+        	word_freqs[word] = word_freqs[word] + 1
+        end
+    end 
+end 
+
+local mt = {}
+
+i = 1
+
+print("\n".."ANTES:\n====================")
+
+for c, v in pairs(word_freqs) do
+        mt[i] = {}
+        mt[i][1] = c
+        mt[i][2] = v
+        print(mt[i][1].." - "..mt[i][2])
+        i = i + 1
+end
+
+print("\n".."DEPOIS:\n====================")
+
+for j = 1, #mt, 1 do
+    for i = 1, #mt-1, 1 do
+        if mt[i][2] < mt[i+1][2] then
+            aux1 = mt[i][1]
+            aux2 = mt[i][2]
+            mt[i][1] = mt[i+1][1]
+            mt[i][2] = mt[i+1][2]
+            mt[i+1][1] = aux1
+            mt[i+1][2] = aux2
+        end
+    end
+end
+
+if (#mt <= 25) then 
+    for i = 1, #mt, 1 do
+        print(i,mt[i][1].." - "..mt[i][2])
+    end
+    else
+        for i = 1, 25, 1 do
+            print(i,mt[i][1].." - "..mt[i][2])
+        end
+end
+
+print("\n")
