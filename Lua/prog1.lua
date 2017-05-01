ad3df23845f36dd7b762deeb7c9670797851423a
Daniela Brazão <daniela.brazao.maksoud@gmail.com>
2017-05-01 13:49:13 -0300

    Update prog1.lua

--- Lua/prog1.lua
+++ Lua/prog1.lua
@@ -3,8 +3,7 @@
 -- =/=/=/=/=/=/=/=/=/=/
 
 local stop_words_file = io.open("../stop_words.txt", "r")
-local stop_words_text = stop_words_file:read"*all"
-
+local stop_words_text = stop_words_file:read("*all")
 stop_words_file:close()
 
 local stop_words = {}
@@ -14,8 +13,7 @@ for word in stop_words_text:gmatch"%w+" do
 end
 
 local test_file = io.open(arg[1], "r")
-local test_text = test_file:read"*all":lower()
-
+local test_text = test_file:read("*all"):lower()
 test_file:close()
 
 local word_freqs = {}  
@@ -30,6 +28,37 @@ for word in test_text:gmatch"%w+" do
     end 
 end 
 
+local mt = {}
+
+i = 1
+
+print("\n".."ANTES:\n====================")
+
 for c, v in pairs(word_freqs) do
-        print(c .. "  -  " .. v)
+        mt[i] = {}
+        mt[i][1] = c
+        mt[i][2] = v
+        print(mt[i][1].." - "..mt[i][2])
+        i = i + 1
 end
+
+print("\n".."DEPOIS:\n====================")
+
+for j = 1, #mt, 1 do
+    for i = 1, #mt-1, 1 do
+        if mt[i][2] < mt[i+1][2] then
+            aux1 = mt[i][1]
+            aux2 = mt[i][2]
+            mt[i][1] = mt[i+1][1]
+            mt[i][2] = mt[i+1][2]
+            mt[i+1][1] = aux1
+            mt[i+1][2] = aux2
+        end
+    end
+end
+
+for i = 1, #mt, 1 do
+    print(mt[i][1].." - "..mt[i][2])
+end
+
+print("\n")
